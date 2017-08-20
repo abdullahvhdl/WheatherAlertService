ce5dde8f23db4aeaaf959e00cb2e15fd7c10cfab
Gürhan Küçük <gurhan.kucuk@yandex.com>
2017-08-20 16:21:36 +0300

    initial commit.

---
+++ src/main/java/com/gurhan/weatheralert/WeatherAlertApplication.java
@@ -0,0 +1,12 @@
+package com.gurhan.weatheralert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherAlertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherAlertApplication.class, args);
+	}
+}
